140c27e4a83876a8ed28ac963ded18caa93498c6
takeshinishikawa <takeshinishikawa@gmail.com>
2022-08-17 15:51:27 -0300

    Add project files.

---
+++ TP_PortfolioCinematografico854/Form1.cs
@@ -0,0 +1,10 @@
+namespace TP_PortfolioCinematografico854
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
