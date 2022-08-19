2c7fb0f120ce5c09e02a92597cddfbeb446be5d4
Luiza Campello <lumcampello@gmail.com>
2022-08-19 10:38:21 -0300

    Initial Login Screen, Create User Repository and changes to User Class

--- Portfolio.Presentation/FrmLogin.Designer.cs
+++ Portfolio.Presentation/FrmLogin.Designer.cs
@@ -29,30 +29,30 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblProjectTitle = new System.Windows.Forms.Label();
             this.lblUsername = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
             this.txbUsername = new System.Windows.Forms.TextBox();
             this.txbPassword = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlLogin = new System.Windows.Forms.Panel();
             this.lblNewAccount = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
+            this.pnlLogin.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblProjectTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Georgia", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.Maroon;
-            this.label1.Location = new System.Drawing.Point(279, 629);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(520, 43);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Portfólio Cinematográfico";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblProjectTitle.AutoSize = true;
+            this.lblProjectTitle.Font = new System.Drawing.Font("Georgia", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblProjectTitle.ForeColor = System.Drawing.Color.Maroon;
+            this.lblProjectTitle.Location = new System.Drawing.Point(289, 608);
+            this.lblProjectTitle.Name = "lblProjectTitle";
+            this.lblProjectTitle.Size = new System.Drawing.Size(520, 43);
+            this.lblProjectTitle.TabIndex = 0;
+            this.lblProjectTitle.Text = "Portfólio Cinematográfico";
+            this.lblProjectTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblUsername
             // 
@@ -95,19 +95,19 @@
             this.txbPassword.TabIndex = 4;
             this.txbPassword.UseSystemPasswordChar = true;
             // 
-            // panel1
+            // pnlLogin
             // 
-            this.panel1.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.panel1.Controls.Add(this.lblNewAccount);
-            this.panel1.Controls.Add(this.txbUsername);
-            this.panel1.Controls.Add(this.btnLogin);
-            this.panel1.Controls.Add(this.txbPassword);
-            this.panel1.Controls.Add(this.lblUsername);
-            this.panel1.Controls.Add(this.lblPassword);
-            this.panel1.Location = new System.Drawing.Point(317, 265);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(430, 315);
-            this.panel1.TabIndex = 5;
+            this.pnlLogin.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.pnlLogin.Controls.Add(this.lblNewAccount);
+            this.pnlLogin.Controls.Add(this.txbUsername);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.txbPassword);
+            this.pnlLogin.Controls.Add(this.lblUsername);
+            this.pnlLogin.Controls.Add(this.lblPassword);
+            this.pnlLogin.Location = new System.Drawing.Point(317, 265);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(430, 315);
+            this.pnlLogin.TabIndex = 5;
             // 
             // lblNewAccount
             // 
@@ -120,6 +120,7 @@
             this.lblNewAccount.TabIndex = 5;
             this.lblNewAccount.Text = "Crie sua conta";
             this.lblNewAccount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNewAccount.Click += new System.EventHandler(this.lblNewAccount_Click);
             // 
             // btnLogin
             // 
@@ -132,6 +133,7 @@
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "Entrar";
             this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // pictureBox1
             // 
@@ -150,13 +152,13 @@
             this.BackColor = System.Drawing.Color.Tan;
             this.ClientSize = new System.Drawing.Size(1064, 681);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlLogin);
+            this.Controls.Add(this.lblProjectTitle);
             this.Name = "FrmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Frm_Login";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -165,12 +167,12 @@
 
         #endregion
 
-        private Label label1;
+        private Label lblProjectTitle;
         private Label lblUsername;
         private Label lblPassword;
         private TextBox txbUsername;
         private TextBox txbPassword;
-        private Panel panel1;
+        private Panel pnlLogin;
         private Label lblNewAccount;
         private Button btnLogin;
         private PictureBox pictureBox1;
