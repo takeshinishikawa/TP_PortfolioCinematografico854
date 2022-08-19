2076bfc6e501632410f3c19c86837a104ab16105
Luiza Campello <lumcampello@gmail.com>
2022-08-19 16:02:49 -0300

    Add Extensions with Enum Description and updates to login screen

--- Portfolio.Presentation/Properties/Resources.Designer.cs
+++ Portfolio.Presentation/Properties/Resources.Designer.cs
@@ -59,7 +59,17 @@ namespace Portfolio.Presentation.Properties {
                 resourceCulture = value;
             }
         }
-      
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _2 {
+            get {
+                object obj = ResourceManager.GetObject("2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
