c484309c418a833e143c612b84fab64c1748cf79
Luiza Campello <lumcampello@gmail.com>
2022-08-24 12:28:14 -0300

    Add new method to PortfolioService

--- Portfolio.Services/PortfolioService.cs
+++ Portfolio.Services/PortfolioService.cs
@@ -1,4 +1,5 @@
 ﻿using Portfolio.Domain;
+using Portfolio.Domain.Enum;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 namespace Portfolio.Services
 {
     public class PortfolioService : IPortfolioService
-    {        
+    {
         public void AddScoreToPortfolio(User loggedUser, Score newScore)
         {
             loggedUser.Portfolio.Add(newScore);
@@ -21,6 +22,30 @@ namespace Portfolio.Services
 
         private void MoviesByCategory(User loggedUser)
         {
+            Dictionary<Category, int> categories = new Dictionary<Category, int>();
+            foreach (Score score in loggedUser.Portfolio)
+            {
+                Category currentCategory = score.Movie.Category;
+
+                if (categories.ContainsKey(currentCategory))
+                {
+                    categories[currentCategory]++;
+                }
+                else
+                {
+                    categories[currentCategory] = 1;
+                }
+            }
+        }
+
+        private void MostWatchedCategory(Dictionary<Category, int> categoriesCount)
+        {
+            
+            foreach (KeyValuePair<Category, int> item in categoriesCount)
+            {
+
+            }
+
 
         }
 
